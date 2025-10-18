a951ff889e8dbd11f92b6c104977f08fc43641e2
oleg-wyzbox <oleg.z@wyzbox.com>
2025-10-18 19:28:41 +0300

    Update VIGILUX policy pages with proper formatting and CSS path updates

---
+++ VIGILUX/ru/Footer.js
@@ -0,0 +1,21 @@
+export class AppFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+                <footer class="footer">
+                    <nav class="footer__nav">
+                        <a class="footer__nav-link" href="billing-policy.html" target="_blank">Политика выставления счетов</a>
+                        <a class="footer__nav-link" href="terms-of-service.html" target="_blank">Условия и положения</a>
+                        <a class="footer__nav-link" href="return-policy.html" target="_blank">Политика возврата</a>
+                        <a class="footer__nav-link" href="privacy-policy.html" target="_blank">Политика конфиденциальности</a>
+                    </nav>
+                     <div class="footer__contact">
+                        <address>Expres Colet SRL, Iași, str. Aurel Vlaicu 78</address>
+                        <address>VAT: RO32088257</address>
+                        <a href="mailto:office.exprescolet@gmail.com">office.exprescolet@gmail.com</a>
+                    </div>
+                </footer>
+            `;
+  }
+}
+
+customElements.define("app-footer", AppFooter);
